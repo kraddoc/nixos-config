ae28ead53828d88e0ee0e02353cb0427306f4d63
kraddoc@crawwingmail.anonaddy.me <kraddoc>
2026-06-19 11:13:42 +0300

    git-push-commit functions for zsh

--- modules/home-manager/shell/zsh/functions.nix
+++ modules/home-manager/shell/zsh/functions.nix
@@ -1,8 +1,26 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  programs.zsh.initContent = 
-    lib.mkOrder 1200 '''
-      echo "Hello zsh initContent!"
-    ''';
+  programs.zsh.initContent = lib.mkOrder 1000 ''
+    # Quick git add, commit, and push
+    gacp() {
+      if [ -z "$1" ]; then
+        echo "Usage: gacp \"Your commit message here\""
+        return 1
+      fi
+      git add .
+      git commit -m "$1"
+      git push
+    }
+
+    # Quick git add and commit (without push)
+    gac() {
+      if [ -z "$1" ]; then
+        echo "Usage: gac \"Your commit message here\""
+        return 1
+      fi
+      git add .
+      git commit -m "$1"
+    }
+    '';
 }
