66e0b7b4d09f428d850581a8aba9e1598193cb1c
kraddoc@crawwingmail.anonaddy.me <kraddoc>
2026-06-24 19:44:04 +0300

    replaced set proton installations with protonplus

--- modules/system/gaming/steam.nix
+++ modules/system/gaming/steam.nix
@@ -4,11 +4,13 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
   };
   
+  # proton settings
+  environment.systemPackages = with pkgs ; [
+    protonplus
+  ];
+
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
 }
